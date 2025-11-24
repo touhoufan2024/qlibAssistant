a24b5d77abf08e4ff9226e79e30cf9b586cb9fdd
touhoufan <touhoufan2024@gmail.com>
2025-11-24 17:42:48 +0800

    aggr: add base code
    Signed-off-by: touhoufan <touhoufan2024@gmail.com>

---
+++ aggr.py
@@ -0,0 +1,35 @@
+import os
+from dataclasses import dataclass
+
+from utils import ConfigLoader
+from executor import Execute, ExecuteYamls
+from updater import DataUpdater, DataUpdaterFromGithub
+from collector import Collector, CollectorMlrunDir
+
+from loguru import logger
+
+
+
+
+@dataclass
+class AggrTasks:
+    def __init__(self, config: ConfigLoader, exec: Execute, data_updater: DataUpdater, collector: Collector):
+        self.config = config
+        self.data_updater = data_updater
+        self.exec = exec
+        self.collector = collector
+        pass
+
+    def run_aggr(self):
+        logger.info("Starting aggregated tasks...")
+        data_updater.update_data()
+
+
+if __name__ == "__main__":
+    config = ConfigLoader()
+    data_updater = DataUpdaterFromGithub(config)
+    executor = ExecuteYamls(config)
+    collector = CollectorMlrunDir(config)
+    aggr_tasks = AggrTasks(config, executor, data_updater, collector)
+    aggr_tasks.run_aggr()
+    pass
