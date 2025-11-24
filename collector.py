2f544a51f47b0499481fe8a6f13f015827e9f37a
touhoufan <touhoufan2024@gmail.com>
2025-11-24 16:54:45 +0800

    collector: base code
    Signed-off-by: touhoufan <touhoufan2024@gmail.com>

--- collector.py
+++ collector.py
@@ -5,6 +5,18 @@ import os
 from loguru import logger
 import requests
 import subprocess
+import utils
+import qlib
+import pandas as pd
+from qlib.constant import REG_CN
+from qlib.utils import init_instance_by_config, flatten_dict
+from qlib.workflow import R
+from qlib.config import C
+from qlib.workflow.record_temp import SignalRecord, PortAnaRecord, SigAnaRecord
+from qlib.tests.data import GetData
+from qlib.tests.config import CSI300_BENCH, CSI300_GBDT_TASK
+
+from qlib.contrib.report import analysis_model, analysis_position
 
 
 # 统计 某个mlrun目录内的结果
@@ -19,16 +31,31 @@ class Collector(ABC):
         pass
 
 
-
-
 class CollectorMlrunDir(Collector):
     def __init__(self, config):
         super().__init__(config)
         self.config = config
+        self.qlib_init("/home/ash/.qlibAssistant/2025-11-24-15/")
+    
+    def qlib_init(self, mlrun_dir = "../mlruns"):
+        provider_uri = "~/.qlib/qlib_data/cn_data"  # target_dir
+        exp_manager = C["exp_manager"]
+        # exp_manager["kwargs"]["uri"] = "file:" + self.config.get_output_folder()
+        exp_manager["kwargs"]["uri"] = "file:" + mlrun_dir
+        qlib.init(provider_uri=provider_uri, region=REG_CN, exp_manager=exp_manager)
+
+    def print_all_exps(self):
+        exps = R.list_experiments()
+
+        for a, b in exps.items():
+            print(a)
+            print(b)
 
     def analysis(self, mlrun_dir = "../mlruns"):
-        pass
+        self.print_all_exps()
 
 
 if __name__ == "__main__":
-    pass
+    cfig = utils.ConfigLoader()
+    coll = CollectorMlrunDir(cfig)
+    coll.analysis("/home/ash/.qlibAssistant/2025-11-24-15/")
