6cbb59f6e5d0cd5f9feb2abcd79627c77a598844
touhoufan <touhoufan2024@gmail.com>
2025-11-26 17:33:17 +0800

    collect: fix show all experiments
    Signed-off-by: touhoufan <touhoufan2024@gmail.com>

--- collector.py
+++ collector.py
@@ -18,6 +18,10 @@ from qlib.tests.config import CSI300_BENCH, CSI300_GBDT_TASK
 
 from qlib.contrib.report import analysis_model, analysis_position
 
+from loguru import logger
+
+from dataclasses import dataclass, field
+from typing import List
 
 # 统计 某个mlrun目录内的结果
 # 重点关注的内容有 label 的排序, 以及性能的排序
@@ -30,11 +34,17 @@ class Collector(ABC):
     def analysis(self):
         pass
 
+@dataclass
+class ExperimentInfo:
+    id: str
+    name: str
+    recorders: List[str] = field(default_factory=list)
 
 class CollectorMlrunDir(Collector):
     def __init__(self, config):
         super().__init__(config)
         self.config = config
+        self.experiment_list: List[ExperimentInfo] = []
     
     def qlib_init(self, mlrun_dir = "../mlruns"):
         logger.info(f"Initializing Qlib with mlrun dir: {mlrun_dir}")
@@ -44,20 +54,27 @@ class CollectorMlrunDir(Collector):
         exp_manager["kwargs"]["uri"] = "file:" + mlrun_dir
         qlib.init(provider_uri=provider_uri, region=REG_CN, exp_manager=exp_manager)
 
-    def print_all_exps(self):
+    def get_all_exps(self):
         exps = R.list_experiments()
-        print(f"Found {len(exps)} experiments:")
-
         for a, b in exps.items():
-            print(a)
-            print(b)
+            exp_info = ExperimentInfo(id=b.id, name=b.name, recorders=b.info['recorders'])
+            self.experiment_list.append(exp_info)
 
-    def analysis(self, mlrun_dir = "../mlruns"):
-        self.qlib_init(self.config.get_output_folder())
-        self.print_all_exps()
+    def show_all_exps(self):
+        logger.info(f"Found {len(self.experiment_list)} experiments:")
+        for exp in self.experiment_list:
+            logger.info(f"Experiment ID: {exp.id}, Name: {exp.name}, Recorders: {exp.recorders}")
+
+    def analysis(self, mlrun_dir=None):
+        if mlrun_dir is None:
+            mlrun_dir = self.config.get_output_folder()
+        logger.info(f"Analyzing mlrun dir: {mlrun_dir} ...")
+        self.qlib_init(mlrun_dir)
+        self.get_all_exps()
+        self.show_all_exps()
 
 
 if __name__ == "__main__":
     cfig = utils.ConfigLoader()
     coll = CollectorMlrunDir(cfig)
-    coll.analysis("/home/ash/.qlibAssistant/2025-11-24-15/")
+    coll.analysis("/home/ash/.qlibAssistant/2025-11-26-16/")
