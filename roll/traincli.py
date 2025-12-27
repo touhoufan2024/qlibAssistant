fb3a018d73a992bd170ea97ad3eaaaa4e50bfb99
touhoufan <touhoufan2024@gmail.com>
2025-12-27 14:47:59 +0800

    train: delete useless func
    Signed-off-by: touhoufan <touhoufan2024@gmail.com>

--- roll/traincli.py
+++ roll/traincli.py
@@ -45,45 +45,14 @@ class TrainCLI:
         self.rolling_gen = RollingGen(step=step, rtype=rolling_type)
         self.trainer = TrainerR(experiment_name=self.exp_name)
 
-    def init(self, model="LGB"):
-        """
-        [警告] 初始化并全量重新训练
-        :param model: 模型配置文件名 (如 LGB, XGB)
-        :param step: 滚动步长
-        """
-        logger.warning(f"!!! 准备重置实验 [{self.exp_name}] 并全量训练 !!!")
-        logger.info(f"模型配置: {model} | 滚动步长: {self.step}")
-        # TODO: inst = RollingTaskExample(experiment_name=self.exp_name, ...)
-        # TODO: inst.reset()
-        # TODO: inst.run()
-
-    def update(self, step=20):
-        """
-        [核心] 增量滚动训练: 仅训练新产生的时间段
-        """
-        logger.info(f"检查实验 [{self.exp_name}] 是否需要增量训练...")
-        # TODO: 比较 R.list_recorders 的 max(test_end_date) 与 数据最新日期
-        need_train = True # 模拟判断
-        if need_train:
-            logger.info("发现新数据，开始训练新的滚动子模型...")
-            # TODO: inst = RollingTaskExample(...)
-            # TODO: inst.task_training(...)
-            logger.success("增量训练完成")
-        else:
-            logger.info("当前模型已是最新的，无需训练。")
-
-    def repair(self, rid):
-        """修复特定 Recorder ID 的训练"""
-        logger.info(f"正在修复/重跑 Recorder ID: [{rid}]...")
-
     def start(self):
         """开始自动滚动训练"""
         logger.info(f"启动滚动训练... step={self.step}")
-        tasks = self.task_generating()
+        tasks = self.gen()
         self.task_training(tasks)
         self.task_collecting()
 
-    def task_generating(self):
+    def gen(self):
         print("========== task_generating ==========")
         tasks = task_generator(
             tasks=self.task_config,
