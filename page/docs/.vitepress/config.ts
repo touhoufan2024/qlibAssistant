d63e8e3bd595407294466d0759960224ec5bff37
okmyworld <2124455076@qq.com>
2026-03-19 14:27:28 +0800

    Update config.ts

--- page/docs/.vitepress/config.ts
+++ page/docs/.vitepress/config.ts
@@ -13,10 +13,6 @@ export default defineConfig({
     ],
     nav: [
       { text: '首页', link: '/' },
-      { text: '文档', link: '/pages/docs/' },
-      { text: '随笔', link: '/pages/essays/' },
-      { text: '马后炮', link: '/pages/mahoupao/' },
-      { text: '帮助', link: '/pages/about/' },
     ],
     sidebar: {
       ...pagesSidebar,
